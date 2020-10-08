1f67f8ed732a6e92ad14f8ffa1075d604dec9d88
Garett <garett.breen@gmail.com>
2020-10-08 15:12:06 -0600

    Fix functions

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "BarcodeScanner",
+    defaultLocalization: "en",
     platforms: [.iOS(.v10)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
--- Sources/Helpers/Functions.swift
+++ Sources/Helpers/Functions.swift
@@ -7,15 +7,9 @@ import AVFoundation
  - Returns: An image.
  */
 func imageNamed(_ name: String) -> UIImage {
-  let cls = BarcodeScannerViewController.self
-  var bundle = Bundle(for: cls)
   let traitCollection = UITraitCollection(displayScale: 3)
 
-  if let resourceBundle = bundle.resourcePath.flatMap({ Bundle(path: $0 + "/BarcodeScanner.bundle") }) {
-    bundle = resourceBundle
-  }
-
-  guard let image = UIImage(named: name, in: bundle, compatibleWith: traitCollection) else {
+    guard let image = UIImage(named: name, in: Bundle.module, compatibleWith: traitCollection) else {
     return UIImage()
   }
 
@@ -28,11 +22,7 @@ func imageNamed(_ name: String) -> UIImage {
  - Returns: An image.
  */
 func localizedString(_ key: String) -> String {
-  if let path = Bundle(for: BarcodeScannerViewController.self).resourcePath,
-    let resourceBundle = Bundle(path: path + "/Localization.bundle") {
-    return resourceBundle.localizedString(forKey: key, value: nil, table: "Localizable")
-  }
-  return key
+    NSLocalizedString(key, bundle: Bundle.module, comment: key)
 }
 
 /// Checks if the app is running in Simulator.
