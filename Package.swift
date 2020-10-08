3f0839fbb582e0987c95b47142f9172264fa0a7f
Garett <garett.breen@gmail.com>
2020-10-08 15:23:57 -0600

    Add assets

--- Package.swift
+++ Package.swift
@@ -22,6 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "BarcodeScanner",
-            path: "Sources"),
+            path: "Sources",
+            resources: [.copy("Images/cameraRotate@3x.png")]),
     ]
 )
